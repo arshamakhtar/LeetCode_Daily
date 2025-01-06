6d421b693f69d5c20a9bd3c0dabb4f6a00890996
arshamakhtar <192856576+arshamakhtar@users.noreply.github.com>
2025-01-06 18:57:07 +0530

    Update 1769.cpp

--- LeetCode/1769.cpp
+++ LeetCode/1769.cpp
@@ -4,11 +4,13 @@ public:
         int n = boxes.length();
         vector<int> ans(n, 0);
         int balls = 0, moves = 0;
+        
         for (int i = 0; i < n; ++i) {
             ans[i] += moves;
             balls += (boxes[i] == '1') ? 1 : 0;
             moves += balls;
         }
+        
         balls = 0, moves = 0;
         for (int i = n - 1; i >= 0; --i) {
             ans[i] += moves;
